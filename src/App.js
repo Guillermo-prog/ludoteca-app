7ae5d77be810752bc8c584b06bf62e5b1ae03531
Guillermo-prog <72439217+Guillermo-prog@users.noreply.github.com>
2023-03-18 21:11:46 +0100

    feat: añadido el contexto de authenticacion de usuarios (#8)
    * feat: añadido el contexto de authenticacion de usuarios
    
    * fix: removes hooks testing lib to fix unit tests
    
    * deleted unnecessary logs and comments

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Login } from "./components/Login";
 import { Home } from "./components/Home";
+import { AuthProvider } from "./context/authContext";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { useAuth } from "../context/authContext";
 
 export function Home() {
-  return <div>Home</div>;
+  const { user } = useAuth(); //accedemos solo al valor user del objeto que devuelve useAuth
+  const loginCheck = user.login.toString();
+  return (
+    <div>
+      <div>Home</div> <div>{loginCheck}</div>
+    </div>
+  );
 }
