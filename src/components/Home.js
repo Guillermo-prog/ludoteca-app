334bf378280b12db95d08914be4c73b6f22bf23e
Guillermo-prog <72439217+Guillermo-prog@users.noreply.github.com>
2023-03-18 21:05:47 +0100

    deleted unnecessary logs and comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,6 @@ import { useAuth } from "../context/authContext";
 
 export function Home() {
   const { user } = useAuth(); //accedemos solo al valor user del objeto que devuelve useAuth
-  console.log(user);
   const loginCheck = user.login.toString();
   return (
     <div>
--- src/context/authContext.js
+++ src/context/authContext.js
@@ -2,15 +2,12 @@ import { createContext, useContext } from "react";
 
 const authContext = createContext();
 
-/*Utilizamos esta funcion para no tener que importar en los componentes
-el objeto context y la el metod useContext*/
 export const useAuth = () => {
   const context = useContext(authContext);
   if (!context) throw new Error("There is not authProvider");
   return context;
 };
 
-//Este componente proporciona el acceso al contexto a los componentes que englobe
 export function AuthProvider({ children }) {
   const user = {
     login: true,
